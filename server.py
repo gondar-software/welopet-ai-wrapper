43d88fbf47719c69267ae957c1040602a9ff92a6
gondar-software <monate615@gmail.com>
2025-04-17 06:15:25 +0000

    add delete pod handler

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from core.pod_manager import create_pod_with_network_volume, get_pod_info
+from core.pod_helper import create_pod_with_network_volume, get_pod_info, delete_pod
 
 if __name__ == "__main__":
     # create_pod_with_network_volume("0mdsp6d0ht", "test1")
-    print(get_pod_info("kplv02tnurj057"))
+    print(delete_pod("kplv02tnurj057"))
